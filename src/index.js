40a1de68e89f6436c242d79dcb05780c782920aa
sonifeg <l.d.i-sfg@hotmail.com>
2022-01-25 18:02:26 -0600

    final

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import validator from './validator.js';
 const botonValidacion= document.getElementById("Btnvalidar")
+const botonValidarOtraVez= document.getElementById("ValidarOtraVez")
 const nombreCliente= document.getElementById("Client")
 const creditCardNumber= document.getElementById("Cardnumber")
-
+const mensajesValido= document.getElementById("mensajesValidacion")
+const mensajesInvalido= document.getElementById("mensajesInvalidacion")
 
 function ValidarNumTarjeta() {
  if(validator.isValid(creditCardNumber.value)==true){
- alert("Tu tarjeta es valida "+ nombreCliente.value);
+     //mensaje valido o invalido
+     mensajesValido.innerHTML="Tu tarjeta es válida " + nombreCliente.value;
  }
- else{
- alert("Tu tarjeta es invalida "+ nombreCliente.value);
- }
-}
+  else{
+    mensajesInvalido.innerHTML="Tu tarjeta es inválida "+ nombreCliente.value;
+  }
  //ocultar con maskify
-  const tarjetaOculta= validator.maskify(creditCardNumber.value);
- 
+ creditCardNumber.value= validator.maskify(creditCardNumber.value);
+}  
+function ValidarNuevamente(){
+//limpiar datos
+nombreCliente.value="";
+creditCardNumber.value="";
+mensajesValido.innerHTML="";
+mensajesInvalido.innerHTML="";
+}
 
-
+//botones con eventos
 botonValidacion.addEventListener("click", ValidarNumTarjeta);
+botonValidarOtraVez.addEventListener("click", ValidarNuevamente);
 
 
  
--- src/validator.js
+++ src/validator.js
@@ -1,42 +1,42 @@
 const validator = {
-  // ...isValid 
+  // ...funcion isValid 
   isValid(creditCardNumber){
-    //Variable donde convertimos el string a array
+    //Variable donde convertimos el string a array y a numero
     let arrayNum= creditCardNumber.split("").reverse().map(Number);
-     //Multiplicar los numeros pares
+     //Multiplicar los numeros pares, si el resto de la longitud mi numero es igual a 0
       if (arrayNum.lenght % 2 == 0){
         arrayNum = arrayNum.map((numero,idx)=> idx % 2 === 0 ? numero * 2 : numero);
       }
         else{
          arrayNum = arrayNum.map((numero,idx)=> idx % 2 === 1 ? numero * 2 : numero);
         }
-       //Sumar los numeroos que pasen de 9
+       //Habiendo multiplicado, si los numeros pasan de 9 restar 9
       arrayNum = arrayNum.map(numero => numero > 9 ? numero -9 : numero);
-       //Sumar todo
+       //Teniendo mis numeros nuevos, sumar todo
       let suma= arrayNum.reduce((acc, numero)=> acc += numero, 0);
-       //validacion de tarjeta
+       //validacion de tarjeta, si el resto de mi suma es 0 valida de lo contrario invalida
     const sumaValor= suma % 10 ===0;
       if (sumaValor){
         true 
-        console.log ("Tu tarjeta es valida")
       } 
        else{
-         false
-         console.log("Tu tarjeta es invalida")       
+         false     
         }
-       console.log(arrayNum);
-       console.log(suma);
-       console.log(sumaValor);
        return sumaValor;
   },
-  // ... maskify
-  maskify(creditCardNumber){
-    
-   if(creditCardNumber.lenght > 5){
-    return "#".repeat(creditCardNumber.lenght -4) + creditCardNumber.substring(creditCardNumber.lenght -4);
-    console.log(creditCardNumber.value)
+  // ... funcion maskify
+  maskify(creditCardNumber) {
+    //Creo la variable que identifique mi longitud
+    var oculto = creditCardNumber.length;
+    //si la longitud de mi numero es menor a 5, regresa el numero de la tarjeta
+    if (creditCardNumber < 5) {
+      return creditCardNumber;
+     //si es mayor a 5, mi variable oculto - valor inicial  menos 5(deja los ultimos 4 descubiertos), sustituye mi valor por #(los numeros anteriores)
+    }else{
+      return creditCardNumber.split('').map(function(val, idx){
+        return (oculto - idx < 5) ? val : '#';
+     }).join('');
+    }
   }
-  return creditCardNumber;
-  },
-}
+};
 export default validator;
